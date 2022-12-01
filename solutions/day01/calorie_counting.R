aa47a0dcf2720f4b2a107dec7590a84ce9ddc6c2
riinuots <riinu.pius@ed.ac.uk>
2022-12-01 13:22:30 +0000

    day 01

---
+++ solutions/day01/calorie_counting.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(scales)
+theme_set(theme_bw())
+
+# Part I
+input_orig = read_csv("solutions/day01/input",
+                      col_names = "calories",
+                      skip_empty_rows = FALSE)
+calories = input_orig %>% 
+  rowid_to_column() %>% 
+  mutate(elf_rowid = if_else(is.na(calories), paste("elf", rowid), NA_character_)) %>% 
+  fill(elf_rowid, .direction = "up") %>% 
+  drop_na() %>% 
+  group_by(elf_rowid) %>% 
+  summarise(total = sum(calories)) %>% 
+  ungroup()
+
+calories %>% 
+  slice_max(total)
+
+# Part II
+
+calories %>% 
+  slice_max(total, n = 3) %>% 
+  summarise(sum(total))
