3e44defe6b5e6bf07d244cab6d1ac817e0e7f75f
riinuots <riinu.pius@ed.ac.uk>
2022-12-04 05:44:16 +0000

    unnecessary line

--- solutions/day04/camp_cleanup.R
+++ solutions/day04/camp_cleanup.R
@@ -3,7 +3,6 @@ library(tidyverse)
 input_orig = read_csv("solutions/day04/input", col_names = c("elf1", "elf2")) 
                  
 camp = input_orig %>% 
-  rowid_to_column("pair_id") %>% 
   separate(elf1, into = c("elf1_start", "elf1_end"), convert = TRUE) %>% 
   separate(elf2, into = c("elf2_start", "elf2_end"), convert = TRUE) 
 
